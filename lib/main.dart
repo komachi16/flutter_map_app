01693f08adb400e6bb6fc3bf7a749b57ddb7d752
komachi16 <haru16.nhs@gmail.com>
2024-12-15 15:02:00 +0900

    Lint対応

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Map App Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
